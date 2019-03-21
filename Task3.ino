35f3957d63e35d563f756e43f0eb3e388507d1d7
MarkyMark11 <47856837+MarkyMark11@users.noreply.github.com>
2019-03-21 15:21:25 +0000

    Update Task3.ino

--- Task3.ino
+++ Task3.ino
@@ -4,6 +4,8 @@ const int turning_radius = 100;
 const int input_RPM = 900;
 const int percent_variation = 10;
 const int timer = 10;
+const int speed_itterations = 10;
+const int circle_itterations = 4; 
 bool add_flag = true;
 
 void setup() {
@@ -12,9 +14,9 @@ Serial.begin(19200);
 }
 
 void loop() {
-  //obj.delta_speed_fun(input_RPM,percent_variation,timer,add_flag);
-  //obj.rotate_counter_clockwise(turning_radius,input_RPM);
+  //obj.delta_speed_fun(input_RPM,percent_variation,timer,add_flag,speed_itterations);
+  obj.rotate_counter_clockwise(turning_radius,input_RPM,circle_itterations);
   //obj.rotate_clockwise(turning_radius,input_RPM);
-  obj.turn_90_L(input_RPM);
+  //obj.turn_90_L(input_RPM);
   //obj.turn_90_R(input_RPM);
 }
