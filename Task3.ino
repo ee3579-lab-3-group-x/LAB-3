8c51c332dd89bafbfee5ac1f198469b67fdb8806
MarkyMark11 <47856837+MarkyMark11@users.noreply.github.com>
2019-03-21 15:30:08 +0000

    Update Task3.ino
    Units need modified

--- Task3.ino
+++ Task3.ino
@@ -6,6 +6,8 @@ const int percent_variation = 10;
 const int timer = 10;
 const int speed_itterations = 10;
 const int circle_itterations = 4; 
+const int turning_angle = 90;
+int RPM_LIMIT = 1000;
 bool add_flag = true;
 
 void setup() {
@@ -14,9 +16,9 @@ Serial.begin(19200);
 }
 
 void loop() {
-  //obj.delta_speed_fun(input_RPM,percent_variation,timer,add_flag,speed_itterations);
+  //obj.delta_speed_fun(input_RPM,percent_variation,timer,add_flag,speed_itterations,RPM_LIMIT);
   obj.rotate_counter_clockwise(turning_radius,input_RPM,circle_itterations);
   //obj.rotate_clockwise(turning_radius,input_RPM);
-  //obj.turn_90_L(input_RPM);
-  //obj.turn_90_R(input_RPM);
+  //obj.turn_L(input_RPM, turning_angle);
+  //obj.turn_R(input_RPM, turning_angle);
 }
